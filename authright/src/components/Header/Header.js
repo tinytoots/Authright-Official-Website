a0820860a2c2c56a587b0f7961b779ad52014291
tinytoots <wangchen2018n@163.com>
2021-06-28 15:32:39 -0400

    first version

---
+++ authright/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is header</h1>
+            </div>
+        )
+    }
+}
