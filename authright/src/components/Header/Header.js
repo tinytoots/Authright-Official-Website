a9bbc2f6800c41d52a1ccac5080f2154d8f58214
tinytoots <wangchen2018n@163.com>
2021-06-28 15:39:41 -0400

    bug fix

--- authright/src/components/Header/Header.js
+++ authright/src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'; 
 
 export default class Header extends Component {
     render() {
         return (
             <div>
                 <h1>this is header</h1>
+                <DatePicker />
             </div>
         )
     }
