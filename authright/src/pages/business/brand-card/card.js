9927fef1f02fe3a1080bd6d93d4e404000512a04
tinytoots <wangchen2018n@163.com>
2021-07-09 15:44:41 -0400

    Add a very cool brand card

---
+++ authright/src/pages/business/brand-card/card.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './card.css'
+
+export default class card extends Component {
+    render() {
+        return (
+            <div className='card-bg'>
+                <h1>This is card</h1>
+                <div className="card">
+                    <div className="card-content">
+                        <h2 className="card-title">Welcome Authright!</h2>
+                        <p className="card-body">This is a great compnay that everyone want be a employee</p>
+                        <a href="#" className="button">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- authright/src/pages/business/business.js
+++ authright/src/pages/business/business.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-// import './business.css'
+import './business.css'
 import BrandBox from './brand-box/box'
+import BrandCard from './brand-card/card'
 
 
 export default class business extends Component {
@@ -8,8 +9,9 @@ export default class business extends Component {
         return (
             <div>
                 <div className='career-bg'>
-                    <h1>This is business page</h1>
+                    <h1 style={{marginBottom: 50}}>This is business page</h1>
                     <BrandBox />
+                    <BrandCard />
                 </div>
             </div>
         )
