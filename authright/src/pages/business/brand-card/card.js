eee262dde7098dc88b3519ada9974d80a38c9efe
tinytoots <wangchen2018n@163.com>
2021-07-12 21:03:12 -0400

    add forgerock and ping page, fix brand box issue

--- authright/src/pages/business/brand-card/card.js
+++ authright/src/pages/business/brand-card/card.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
+import { Row, Col, Divider } from 'antd';
+import 'antd/dist/antd.css';
 import './card.css'
 
 export default class card extends Component {
     render() {
         return (
             <div className='card-bg'>
-                <h1>This is card</h1>
-                <div className="card">
-                    <div className="card-content">
-                        <h2 className="card-title">Welcome Authright!</h2>
-                        <p className="card-body">This is a great compnay that everyone want be a employee</p>
-                        <a href="/home" className="button">Learn More</a>
+                {/* <h1>This is card</h1> */}
+                <Row gutter={16}>
+                    <Col className="gutter-row" span={6}>
+                    <div className="card">
+                        <div className="card-content">
+                            <h2 className="card-title">Welcome Authright!</h2>
+                            <p className="card-body">This is a great compnay that everyone want be a employee</p>
+                            <a href="/home" className="button">Learn More</a>
+                        </div>
                     </div>
-                </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                    <div className="card">
+                        <div className="card-content">
+                            <h2 className="card-title">Welcome Authright!</h2>
+                            <p className="card-body">This is a great compnay that everyone want be a employee</p>
+                            <a href="/home" className="button">Learn More</a>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                    <div className="card">
+                        <div className="card-content">
+                            <h2 className="card-title">Welcome Authright!</h2>
+                            <p className="card-body">This is a great compnay that everyone want be a employee</p>
+                            <a href="/home" className="button">Learn More</a>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                    <div className="card">
+                        <div className="card-content">
+                            <h2 className="card-title">Welcome Authright!</h2>
+                            <p className="card-body">This is a great compnay that everyone want be a employee</p>
+                            <a href="/home" className="button">Learn More</a>
+                        </div>
+                    </div>
+                    </Col>
+                </Row>
+                
             </div>
         )
     }
--- authright/src/pages/business/business.js
+++ authright/src/pages/business/business.js
@@ -3,14 +3,18 @@ import './business.css'
 import BrandBox from './brand-box/box'
 import BrandCard from './brand-card/card'
 import ExpandCard from './brand-expandCard/expandCard'
+import UnderLine from '../../img/Underline.svg';
 
 
 export default class business extends Component {
     render() {
         return (
             <div>
-                <div className='career-bg'>
-                    <h1 style={{marginBottom: 50}}>This is business page</h1>
+                <div className='business-bg'>
+                    <div className="businessOutcome-title1">We save a lot of money</div>
+                    <div className="businessOutcome-title2">for many of the world's</div>
+                    <div className="businessOutcome-title2">top 500 companies</div>
+                    <img className='businessOutcome-underline-img' src={UnderLine} width={245} />
                     <BrandBox />
                     <BrandCard />
                     <ExpandCard />
