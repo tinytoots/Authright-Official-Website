adbd953acd68ce00bf44e2fc505b6f470c2029bd
tinytoots <wangchen2018n@163.com>
2021-08-13 12:41:52 -0400

    home page fix

--- authright/src/pages/home/intro/intro.js
+++ authright/src/pages/home/intro/intro.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './intro.css';
 import { Divider } from 'antd';
+import 'antd/dist/antd.css';
 import BostonBg from '../../../img/boston.jpg';
 
 // var bgImg = {
@@ -13,14 +14,15 @@ export default class intro extends Component {
         return (
             <div className='intro-bg'>
                 <h1 />
-                <div className='company-title'>
+                <div className='company-name-title'>
                     AUTHRIGHT
                 </div>
-                <div className='gray-lane' />
-                <div className='company-content1'>
+                <div className='gray-lane-intro'>
+                </div>
+                <div className='company-name-content1'>
                     An Innovative Service Company 
                 </div>
-                <div className='company-content2'>
+                <div className='company-name-content2'>
                     Consulting, Solutions, Training
                 </div>
             </div>
