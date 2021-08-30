0c95da92c820c134d3bcc7c91308ffd801bc1aea
XuTao <xu.tao@husky.neu.edu>
2021-08-30 13:52:05 -0400

    homepage-title-modify

--- authright/src/pages/home/advantage/advantage.js
+++ authright/src/pages/home/advantage/advantage.js
@@ -10,7 +10,7 @@ export default class advantage extends Component {
                 <div className='advan'>Whether you want to integrate IAM for your own system or need a high-quality development team to help you turn your ideas into reality, we can provide you with the reliable technical services</div>
                 <NavLink to="/about-us">
                     <div className='unifiedcontrol-card-title'>
-                        Learn more >
+                        Learn more {">"}
                         <i className='approw-arrow-down-line arrow'></i>
                     </div>
                 </NavLink>
