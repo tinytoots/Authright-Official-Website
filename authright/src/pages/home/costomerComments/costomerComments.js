3eb2fcb14fb0af3c08dba1f7d9542f0232acc0ba
tinytoots <wangchen2018n@163.com>
2021-08-31 12:25:34 -0400

    update customer comments page

--- authright/src/pages/home/costomerComments/costomerComments.js
+++ authright/src/pages/home/costomerComments/costomerComments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './costomerComments.css'
 import Tilt from 'react-vanilla-tilt'
 import { Row, Col, Divider } from 'antd';
+import { NavLink } from 'react-router-dom'
 import ForgerockCEOImag from '../../../img/forgerock-ceo2.jpg'
 import PingCEOImag from '../../../img/ping-ceo.jpg' 
 import BrandTrust from '../../business/brand-trust/trust'
@@ -9,6 +10,8 @@ import BrandTrust from '../../business/brand-trust/trust'
 // import HsbcLogo from '../../../../../img/case-home-depot.svg'
 // import AuthrightLogo from '../../../../img/authright-logo.jpg'
 import HSBC from '../../../img/comments-hsbc.png'
+import HomeDepot from '../../../img/comments-Deloitte.png'
+import Mohawk from '../../../img/comments-Mohawk.png'
 import Deloitte from '../../../img/deloitte-2.svg'
 import Toyota from '../../../img/toyota-1.svg'
 
@@ -71,41 +74,48 @@ export default class costomerComments extends Component {
                         </Row> */}
                     </div>
             <div className="container-youtube" style={{height: 300, color: 'white'}}>
-                    <Tilt className="tiltTest" options={{ max: 25, speed: 400}} data-tilt="false">
+                    {/* <Tilt className="tiltTest" options={{ max: 25, speed: 400}} data-tilt="false"> */}
                     <div className="card-youtube" data-tilt data-tilt-max="50" data-tilt-speed="400" data-tilt-perspective="500">
                         <div className="content-youtube">
+                            <img className='comments-logo' src={HSBC} alt = 'logo' /> 
+                            <br />
                             <img src={ForgerockCEOImag} width={60} alt = 'logo' className='customerComments-forgerockCeo-img' />    
-                            {/* <h2>01</h2> */}
+                            <h2>01</h2>
                             <h3>Fran Rosch</h3>
                             <h4>CEO of Forgerock</h4>
                             <p>"As one of our premier ‘go to’ partners, Identity Fusion has proven themselves time and time again to handle the really tough projects."</p>
-                            <a href="#">Read More</a>
-                        </div>
-                    </div>
-                    </Tilt>
-                    {/* <Tilt options={{ scale: 1, max: 52 }}> */}
-                    <div className="card-youtube">
-                        <div className="content-youtube">
-                            {/* <h2>02</h2> */}
-                            <img src={PingCEOImag} width={60} alt = 'logo' className='customerComments-forgerockCeo-img' />    
-                            <h3>Kris Nagel</h3>
-                            <h4>CEO of PingIdentity</h4>
-                            <p>"Authright thrives in solving real world security and identity problems with a calming seasoned leadership approach that few can match."</p>
-                            <a href="#">Read More</a>
+                            <a href="/hsbc">Read More</a>
                         </div>
                     </div>
                     {/* </Tilt> */}
                     {/* <Tilt options={{ scale: 1, max: 52 }}> */}
                     <div className="card-youtube">
                         <div className="content-youtube">
-                            <img className='comments-logo' src={HSBC} alt = 'logo' /> 
+                            <img className='comments-logo' src={HomeDepot} alt = 'logo' /> 
                             <br />
-                            {/* <h2>03</h2> */}
+                            <h2>02</h2>
+                            <img src={PingCEOImag} width={60} alt = 'logo' className='customerComments-forgerockCeo-img' />    
+                            <h3>Kris Nagel</h3>
+                            <h4>CEO of PingIdentity</h4>
+                            <p>"Authright thrives in solving real world security and identity problems with a calming seasoned leadership approach that few can match."</p>
+                            <a href="/deloitte">Read More</a>
+                        </div>
+                    </div>
+                    {/* </Tilt> */}
+                    {/* <Tilt options={{ scale: 1, max: 52 }}> */}
+                    <div className="card-youtube">
+                        <div className="content-youtube">
+                            <img className='comments-logo' src={Mohawk} alt = 'logo' /> 
+                            <br />
+                            <h2>03</h2>
                             <img src={ForgerockCEOImag} width={60} alt = 'logo' className='customerComments-forgerockCeo-img' />    
                             <h3>Tim Cook</h3>
                             <h4>CEO of Apple</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do test tempor incididunt, ut labore et dolore magna aliqua.</p>
-                            <a href="#">Read More</a>
+                            {/* <NavLink to="/mohawk-industries">
+                                <a href="#">Read More</a>
+                            </NavLink> */}
+                            <a href="/mohawk-industries">Read More</a>
                         </div>
                     </div>
                     {/* </Tilt> */}
