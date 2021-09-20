70828e400e76228c4a70411cdff725c987dfa9f8
XuTao <xu.tao@husky.neu.edu>
2021-09-20 17:45:56 -0400

    modify and add  career  page

--- authright/src/pages/career/careersIam/careersIam.js
+++ authright/src/pages/career/careersIam/careersIam.js
@@ -34,7 +34,7 @@ export default class careersIam extends Component {
                         <Col className="gutter-row" span={16}>
                             <a
                                 className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
+                                href='/careers/detail/iam'
                                 target='_blank'
                                 rel='noopener noreferrer'
                             >
@@ -54,7 +54,7 @@ export default class careersIam extends Component {
                         <Col className="gutter-row" span={16}>
                             <a
                                 className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
+                                href='/careers/detail/forgerock'
                                 target='_blank'
                                 rel='noopener noreferrer'
                             >
@@ -74,7 +74,7 @@ export default class careersIam extends Component {
                         <Col className="gutter-row" span={16}>
                             <a
                                 className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
+                                href='/careers/detail/ping'
                                 target='_blank'
                                 rel='noopener noreferrer'
                             >
@@ -94,7 +94,7 @@ export default class careersIam extends Component {
                         <Col className="gutter-row" span={16}>
                             <a
                                 className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
+                                href='/careers/detail/intern'
                                 target='_blank'
                                 rel='noopener noreferrer'
                             >
@@ -114,124 +114,7 @@ export default class careersIam extends Component {
                         </Row>
                     </div>
 
-                    <div className='careers-title'>
-                        Full Stack Developer
-                    </div>
-                    <div className='solution-gray-lane2' />
-                    <div className="forgerock-conent-bg">
-                        <Row gutter={16}>
-                        <Col className="gutter-row" span={16}>
-                            <div className='careers-position-title'>
-                                Position
-                            </div>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='careers-position-title'>
-                                Team
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        <Col className="gutter-row" span={16}>
-                            <a
-                                className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                            
-                                <div className='unifiedcontrol-card-title2'>
-                                    Senior Full-Stack Developer
-                                    <i className='approw-arrow-down-line arrow'></i>
-                                </div>
-                            </a>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='solution-forgerock-intro'>
-                                Full Stack
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        <Col className="gutter-row" span={16}>
-                            <a
-                                className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                            
-                                <div className='unifiedcontrol-card-title2'>
-                                    Junior-Mid level Full-Stack Developer
-                                    <i className='approw-arrow-down-line arrow'></i>
-                                </div>
-                            </a>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='solution-forgerock-intro'>
-                                Full Stack
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        <Col className="gutter-row" span={16}>
-                            <a
-                                className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                            
-                                <div className='unifiedcontrol-card-title2'>
-                                    Full-Stack Intern 
-                                    <i className='approw-arrow-down-line arrow'></i>
-                                </div>
-                            </a>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='solution-forgerock-intro'>
-                                Full Stack
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        </Row>
-                    </div>
-                    <div className='careers-title'>
-                        Mentorship
-                    </div>
-                    <div className='solution-gray-lane2' />
-                    <div className="forgerock-conent-bg">
-                        <Row gutter={16}>
-                        <Col className="gutter-row" span={16}>
-                            <div className='careers-position-title'>
-                                Position
-                            </div>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='careers-position-title'>
-                                Team
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        <Col className="gutter-row" span={16}>
-                            <a
-                                className='unifiedcontrol-card-default'
-                                href='https://docs.approw.com/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                            >
-                            
-                                <div className='unifiedcontrol-card-title2'>
-                                    Mentorship
-                                    <i className='approw-arrow-down-line arrow'></i>
-                                </div>
-                            </a>
-                        </Col>
-                        <Col className="gutter-row" span={8}>
-                            <div className='solution-forgerock-intro'>
-                                IAM <span style={{color: "#8c8c8c", fontSize: 25}}>|</span> Full Stack
-                            </div>
-                        </Col>
-                        <div className='solution-gray-lane' />
-                        </Row>
-                    </div>
+                    
                 </div>
             </div>
         )
---
+++ authright/src/pages/career/jobDesc/jobDescData.js
@@ -0,0 +1,195 @@
+const jobDescription = {
+    iam: {
+        title: "Identity & Access Management Software Engineer",
+        subTitle: "Identity & Access Management Software Engineer (All Levels)",
+        Location: "Dedham, MA **20-40% Travel Required**",
+        Company: "Authright Inc",
+        PostingTitle: "Identity & Access Management Software Engineer (All Levels)",
+        JobID: "ITSS2021001",
+        RecruitingTeam: "IT Solutions & Services Department",
+        WhoWeAre: 
+        [
+            "Authright, founded in 2014 in Massachusetts, is an information technology company that providing information security including enterprise Identity & Access Management (IAM) Solutions to our customers. Jan 18, 2018, AuthRight and ForgeRock announced Delivery and Strategic Partnership to dedicate to provide top information security solution in the North American area and global in future. ",
+            "We provide high-quality tailored IT consulting and management services to help clients to protect, connect, and manage the digital identities of people, devices, and the internet of things (IoT) through an all-in-one Identify Platform. Beyond the professional service, Authright independent researchers and development of security & protection products include Cloud AM, CASB, SSO and multi-factor login solution."
+        ],
+        PositionSummary: "This position is providing Authright IT professional service for customers. IT Solutions & Services Department diverse group of technical architects, engineers, consultants, and support professionals dedicated to delivering on Authright’s mission of helping our customers to embrace cutting edge technology and propelling our customers’ business imperatives toward success.",
+        Responsibilities: [
+            "You will leverage your experience in Identity and Access Management (IAM), Identity & Access Governance (IAG), Privileged Access Management (PAM), or a related competency to implement the ideal solutions for our clients. You will design, develop, and implement connectors and other technical components for authentication, authorization, identity provisioning, physical security, and audit and monitoring solutions for our information security deployments."
+        ],
+        Requirements: [
+            "Experience implementing enterprise Identity and Access Management and security solutions (e.g., SailPoint, CA, IBM, BMC, Sun, Oracle, ForgeRock, CyberArk, etc.) in client environments",
+            "Experience integrating IAM applications with common identity stores (LDAP, relational databases, application servers, virtual directory servers, physical access management systems)",
+            "Exposure to a variety of technologies such as multi-tiered applications, databases, LDAP and directory services, application servers, network infrastructure, and understanding security and dataflow within these components",
+            "Experience configuring, developing, and deploying scalable enterprise applications (and COTS products) within multiple application server, operating system and database environments (e.g. WebLogic, WebSphere, JBoss, Tomcat on Windows, Linux, UNIX and Oracle, SQL Server, DB2, MySQL)",
+            "Strong hands-on Java/JEE experience",
+            "Good knowledge of software development principles, and design patterns",
+            "Self-starter, fast learner, and flexible to support new technologies as well as legacy applications",
+            "Travel may be required",
+            "BS in Computer Science, Computer Engineering, or related discipline from a top school"
+        ],
+        Benfits: [
+            "Competitive salary with opportunities for raises and bonuses every year",
+            "Visa sponsorship, Green Card Sponsorship available for competitive candidates",
+            "PPO Health, Visual, & Dental Insurance.",
+            "401K retirement plan",
+            "Paid Time Off",
+            "Professional development allowance for conferences, classes, and special events"
+        ],
+        apply: "If you are interested in this position, please send your latest resume and cover letter to hr@authright.com"
+    },
+    forgerock: {
+        title: "ForgeRock (IAM) Identity Technical Architect",
+        subTitle: "ForgeRock (IAM) Identity Technical Architect",
+        Location: "Dedham, MA **20-40% Travel Required**",
+        Company: "Authright Inc",
+        PostingTitle: "ForgeRock (IAM) Identity Technical Architect",
+        JobID: "ITSS2021002",
+        RecruitingTeam: "IT Solutions & Services Department",
+        WhoWeAre: 
+        [
+            "Authright, founded in 2014 in Massachusetts, is an information technology company that providing information security including enterprise Identity & Access Management (IAM) Solutions to our customers. Jan 18, 2018, AuthRight and ForgeRock announced Delivery and Strategic Partnership to dedicate to provide top information security solution in the North American area and global in future. ",
+            "We provide high-quality tailored IT consulting and management services to help clients to protect, connect, and manage the digital identities of people, devices, and the internet of things (IoT) through an all-in-one Identify Platform. Beyond the professional service, Authright independent researchers and development of security & protection products include Cloud AM, CASB, SSO and multi-factor login solution."
+        ],
+        PositionSummary: "In this Identity and Access Management Technical Architect position you will be the primary customer interface and technical authority with regard to ForgeRock IAM products and solutions. This position encompasses the entire life-cycle of an identity management deployment and consists of: requirements definition, design, implementation, testing, go-live, and transition to support. This is both an architectural and technical role and requires high level business acumen coupled with deep technical knowledge of identity management products and system design principles.",
+        Responsibilities: [
+            "Articulate the business and technical benefits of identity management solutions",
+            "Create and define business, functional and application architecture designs as per business requirements",
+            "Architect identity management solutions involving user provisioning, enterprise directories, and web single sign-on using best practices and proven methods",
+            "Deliver internal and customer-facing projects utilizing products within the ForgeRock Solutions",
+            "Create customer-facing documentation including architectural design, requirements, and test plans",
+            "Perform technical training and identity workshops either in-person or remotely",
+            "Provide ongoing support to an existing customer base",
+            "Work with the support team to define, design, implement, and maintain the tools necessary for providing support to our customer base. This includes applications, scripts, and/or tools used by the support team for gathering, diagnosing, or otherwise providing support of the customer’s operating system and/or identity management applications and/or connected systems."
+        ],
+        Requirements: [
+            "Understanding of federation using SAML2 and OAuth2/OpenID Connect solutions",
+            "Understanding of LDAP directory servers and their place in identity management solutions",
+            "Working knowledge of a compiled programming language (Java development a plus)",
+            "Working knowledge of HTML, CSS and JavaScript",
+            "Ability to troubleshoot and identify complex software, hardware, and systems issues",
+            "Extensive networking experience",
+            "Ability to work with others in a distributed environment",
+            "Ability to maintain professionalism while under pressure",
+            "Ability to work within a distributed team across multiple time zones",
+            "Desire to continue to develop personal skill set in a proactive environment",
+            "Values teamwork and can contribute to as well as lead the group",
+            "Ability to exercise judgment in the absence of defined procedures and practices to determine appropriate actions",
+            "Excellent analytical abilities, resulting in quick problem resolution",
+            "Excellent verbal and written communication skills",
+            "Experience with virtual environments (such as Amazon EC2)",
+            "Familiarity with major enterprise solutions such as ERP, CRM, etc",
+            "Experience with Linux/Unix scripting technologies (shell, Perl, etc.)",
+            "Experience with ForgeRock",
+            "Hands-on experience implementing the following: Web SSO products, User provisioning solutions, Enterprise directory solutions"
+        ],
+        Benfits: [
+            "Competitive salary with opportunities for raises and bonuses every year",
+            "Visa sponsorship, Green Card Sponsorship available for competitive candidates",
+            "PPO Health, Visual, & Dental Insurance.",
+            "401K retirement plan",
+            "Paid Time Off",
+            "Professional development allowance for conferences, classes, and special events"
+        ],
+        apply: "If you are interested in this position, please send your latest resume and cover letter to hr@authright.com"
+    },
+    ping: {
+        title: "Ping Identity (IAM) Identity Technical Architect",
+        subTitle: "Ping Identity (IAM) Identity Technical Architect ",
+        Location: "Dedham, MA **20-40% Travel Required**",
+        Company: "Authright Inc",
+        PostingTitle: "Ping Identity (IAM) Identity Technical Architect",
+        JobID: "ITSS2021003",
+        RecruitingTeam: "IT Solutions & Services Department",
+        WhoWeAre: 
+        [
+            "Authright, founded in 2014 in Massachusetts, is an information technology company that providing information security including enterprise Identity & Access Management (IAM) Solutions to our customers. Jan 18, 2018, AuthRight and Ping Identity announced Delivery and Strategic Partnership to dedicate to provide top information security solution in the North American area and global in future. ",
+            "We provide high-quality tailored IT consulting and management services to help clients to protect, connect, and manage the digital identities of people, devices, and the internet of things (IoT) through an all-in-one Identify Platform. Beyond the professional service, Authright independent researchers and development of security & protection products include Cloud AM, CASB, SSO and multi-factor login solution."
+        ],
+        PositionSummary: "In this Identity and Access Management Technical Architect position you will be the primary customer interface and technical authority with regard to Ping Identity IAM products and solutions. This position encompasses the entire life-cycle of an identity management deployment and consists of: requirements definition, design, implementation, testing, go-live, and transition to support. This is both an architectural and technical role and requires high level business acumen coupled with deep technical knowledge of identity management products and system design principles.",
+        Responsibilities: [
+            "Articulate the business and technical benefits of identity management solutions",
+            "Create and define business, functional and application architecture designs as per business requirements",
+            "Architect identity management solutions involving user provisioning, enterprise directories, and web single sign-on using best practices and proven methods",
+            "Deliver internal and customer-facing projects utilizing products within the Ping Identity Solutions",
+            "Create customer-facing documentation including architectural design, requirements, and test plans",
+            "Perform technical training and identity workshops either in-person or remotely",
+            "Provide ongoing support to an existing customer base",
+            "Work with the support team to define, design, implement, and maintain the tools necessary for providing support to our customer base. This includes applications, scripts, and/or tools used by the support team for gathering, diagnosing, or otherwise providing support of the customer’s operating system and/or identity management applications and/or connected systems."
+        ],
+        Requirements: [
+            "Understanding of federation using SAML2 and OAuth2/OpenID Connect solutions",
+            "Understanding of LDAP directory servers and their place in identity management solutions",
+            "Working knowledge of a compiled programming language (Java development a plus)",
+            "Working knowledge of HTML, CSS and JavaScript",
+            "Ability to troubleshoot and identify complex software, hardware, and systems issues",
+            "Extensive networking experience",
+            "Ability to work with others in a distributed environment",
+            "Ability to maintain professionalism while under pressure",
+            "Ability to work within a distributed team across multiple time zones",
+            "Desire to continue to develop personal skill set in a proactive environment",
+            "Values teamwork and can contribute to as well as lead the group",
+            "Ability to exercise judgment in the absence of defined procedures and practices to determine appropriate actions",
+            "Excellent analytical abilities, resulting in quick problem resolution",
+            "Excellent verbal and written communication skills",
+            "Experience with virtual environments (such as Amazon EC2)",
+            "Familiarity with major enterprise solutions such as ERP, CRM, etc",
+            "Experience with Linux/Unix scripting technologies (shell, Perl, etc.)",
+            "Experience with Ping Identity",
+            "Hands-on experience implementing the following: Web SSO products, User provisioning solutions, Enterprise directory solutions"
+        ],
+        Benfits: [
+            "Competitive salary with opportunities for raises and bonuses every year",
+            "Visa sponsorship, Green Card Sponsorship available for competitive candidates",
+            "PPO Health, Visual, & Dental Insurance.",
+            "401K retirement plan",
+            "Paid Time Off",
+            "Professional development allowance for conferences, classes, and special events"
+        ],
+        apply: "If you are interested in this position, please send your latest resume and cover letter to hr@authright.com"
+    },
+    intern: {
+        title: "Cyber Security / Information Security Intern",
+        subTitle: "Cyber Security / Information Security Intern  (non-paid)",
+        Location: "Dedham, MA **20-40% Travel Required**",
+        Company: "Authright Inc",
+        PostingTitle: "Cyber Security / Information Security Intern",
+        JobID: "ITSS2021004",
+        RecruitingTeam: "IT Solutions & Services Department",
+        WhoWeAre: 
+        [
+            "Authright, established in December 2014, in Massachusetts, is a fast-growing information technology company. Our core businesses are independent research and development of security & protection products and the professional IT & Data Services for a variety of customers, ranging from startups to enterprises.",
+            "Authright has developed several information security products, such as X-Factor Login Cloud, Cloudsea, CloudSecutag, etc. The featured product X-Factor Login Cloud allows the web and mobile applications to provide 2- factor login. Our signature service, the Incubation & Development Network allows customers to define and build a tailor-made IT product from the ground up with minimum cost. The team of our talented engineers works closely with customers to understand their needs and provide services of designing, implementing and developing the back-end data solutions and software products. Authright is actively expanding the business into cloud storage, data integration, big data and cloud computing, thriving to provide customers the best solutions for their projects and support to troubleshoot the mission-critical issues."
+        ],
+        PositionSummary: "Mentored by engineers and scientists that are experts in Security fields. From design to operations, you will learn to apply security engineering expertise, innovative concepts, and technology solutions to address every facet of our sponsor’s cyber challenges in three key strategic areas: threat-based operations, threat-based engineering, and security of evolving and emerging technologies.",
+        Responsibilities: [
+            "BS in Computer Science, Computer Engineering, or related discipline from a top school",
+            "You have a passion for security.",
+            "You love to solve problems with efficient, solid code and prove it through automated tests.",
+            "You have designed, built, tested, owned and monitored a service in production.",
+            "You are excited by shipping new features and know that delivering to production takes more than just coding.",
+            "You strive to understand the big picture and given the choice, you would prefer to collaborate with product owners, architects, quality assurance and infrastructure than to go it alone.",
+            "You are continuously growing your skillset."
+        ],
+        Requirements: [
+            "Strong sense of ownership, passionate, fast learner, detail oriented, analytical mindset, perfection seeker.",
+            "At least 5 years hands-on experience in software development.",
+            "Knowledge and familiarity with Unix based OS.",
+            "Solid knowledge of object oriented programming principles and design patterns.",
+            "Solid and demonstrable experience with multi-threaded, concurrent applications.",
+            "Advanced experience in Java.",
+            "Experience with Authentication and Authorization approaches.",
+            "Experience with at least two of the following: SAML, OAuth, OIDC, Redis, JWT tokens",
+            "Experience in Java, .NET programming language (F#, Haskell, etc.) or willing to learn.",
+            "Experience with continuous integration platforms such as Jenkins or Bamboo and testing strategies"
+        ],
+        Benfits: [
+            "Intern to Full-Time Hire opportunity",
+            "Comprehensive training",
+            "Visa sponsorship available for competitive candidates",
+            "Career path consulting"
+        ],
+        apply: "If you are interested in this position, please send your latest resume and cover letter to hr@authright.com"
+    }
+}
+
+export default jobDescription;
