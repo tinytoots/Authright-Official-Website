b4b245a183d568c2b8b65acb91ae468e7bcbbb97
Shujian CHEN <csj244911099@gmail.com>
2021-09-23 15:54:46 -0400

    ceo sectiono

--- authright/src/pages/about/leadership/leadership.js
+++ authright/src/pages/about/leadership/leadership.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import ExpandCard from "../expandCard/expandCard";
 import { employeeInfo as employees } from "../../../constant/employeeInfo";
+import { leadInfo as leads } from "../../../constant/leadInfo";
+import LeadCard from "./leadCard";
 import "./leadership.css";
-import { Row, Col } from "antd";
 import "antd/dist/antd.css";
-
-//const style = { background: "#0092ff", padding: "8px 0" };
 
 export default function Leadership(props) {
   return (
@@ -15,6 +14,9 @@ export default function Leadership(props) {
         <div className="about-leadership-content">
           We are a team. A great journey begins here.
         </div>
+      </div>
+      <div className="about-leadership-ceo">
+        <LeadCard leadInfo={leads}/>
       </div>
       <div className="leadership-container">
         {employees.map((employee) => {
@@ -30,82 +32,6 @@ export default function Leadership(props) {
         })}
       </div>
 
-      {/* <Row gutter={16}>
-          <Col
-            className="gutter-row"
-            span={12}
-            style={{ paddingLeft: 370, paddingBottom: 100 }}
-          >
-            <ExpandCard/>
-          </Col>
-          <Col
-            className="gutter-row"
-            span={12}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-          <Col
-            className="gutter-row"
-            span={6}
-            style={{ paddingLeft: 65, paddingBottom: 100 }}
-          >
-            <ExpandCard />
-          </Col>
-        </Row> */}
     </div>
   );
 }
