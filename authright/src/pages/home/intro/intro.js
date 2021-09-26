213a039b02602534fb4f80140d1c0c09df66444b
Authshujianchen <shujian.chen@authright.com>
2021-09-26 01:39:01 -0400

    testimony data

--- authright/src/pages/home/intro/intro.js
+++ authright/src/pages/home/intro/intro.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import './intro.css';
-import 'antd/dist/antd.css';
+import React, { Component } from "react";
+import "./intro.css";
+import "antd/dist/antd.css";
 
 export default class intro extends Component {
-    render() {
-        return (
-            <div className='intro-bg' style={{minWidth: 1200}}>
-                <div className="intro-text">
-                    <div className='company-name-title'>
-                        AUTHRIGHT
-                    </div>
-                
-                    <div className='company-name-content1'>
-                        <i>An Identity Company </i>
-                    </div>
-                    <div className='company-name-content2'>
-                        Be True, Be Right
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="intro-bg" style={{ minWidth: 1200 }}>
+        <div className="intro-text">
+          <div className="company-name-title">AUTHRIGHT</div>
+
+          <div className="company-name-content1">
+            <i>An Identity Company </i>
+          </div>
+          <div className="company-name-content2">Be True, Be Right</div>
+        </div>
+      </div>
+    );
+  }
 }
