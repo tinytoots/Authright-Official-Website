3fb598f9a1aa9a1647916a354fd036fa4794ae70
Shujian CHEN <csj244911099@gmail.com>
2021-09-30 15:41:01 -0400

    home intro title

--- authright/src/pages/home/intro/intro.js
+++ authright/src/pages/home/intro/intro.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import './intro.css';
-import 'antd/dist/antd.css';
+import React, { Component } from "react";
+import "./intro.css";
+import "antd/dist/antd.css";
 
 export default class intro extends Component {
-    render() {
-        return (
-            <div className='intro-bg'>
-                <div className="authright-inner">
-                    <div className="intro-text">
-                        <div className='company-name-title'>
-                            AUTHRIGHT
-                        </div>
-                    
-                        <div className='company-name-content1'>
-                            <i>An Identity Company </i>
-                        </div>
-                        <div className='company-name-content2'>
-                            Be True,&nbsp;Be Right
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="intro-bg">
+        <div className="authright-inner">
+          <div className="intro-text">
+            <div className="company-name-title">AUTHRIGHT</div>
+
+            <div className="company-name-content1">
+              <i>An Identity Company </i>
             </div>
-        )
-    }
+            <div className="company-name-content2">Be True,&nbsp;Be Right</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
