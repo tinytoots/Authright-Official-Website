9fb1c03417a85b69cc3e193071204771b9b82922
Shujian CHEN <csj244911099@gmail.com>
2021-10-06 16:16:28 -0400

    logo page updated

--- authright/src/pages/about/certification/certCard.js
+++ authright/src/pages/about/certification/certCard.js
@@ -4,17 +4,26 @@ import "./certCard.css";
 
 const CertCard = (props) => {
   const { name, content, color, pic } = props;
+  console.log(pic);
   return (
-    <div
-      className="certCard-container"
-      style={{ borderTop: `5px solid ${color}` }}
-    >
-      <div className="certCard-title">{name}</div>
-      <img src={pic} alt=''/>
+    <div className="certCard-container">
+      <div className="certCard-header">
+        {/* <img className="certCard-img" src={pic} alt={`${name}`} /> */}
+        <div className="certCard-title">{name}</div>
+        <div className="certCard-divider" style={{ borderBottom: `5px solid ${color}` }}></div>
+      </div>
+
       <div className="certCard-subList">
         {content.map((certInfo) => {
-          const { certName, number } = certInfo;
-          return <SubCertCard certName={certName} number={number} />;
+          const { certName, number, pic } = certInfo;
+          return (
+            <SubCertCard
+              key={`${certName}+${number}`}
+              certName={certName}
+              number={number}
+              pic={pic}
+            />
+          );
         })}
       </div>
       <div className="certCard-certName">{}</div>
