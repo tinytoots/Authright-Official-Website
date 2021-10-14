d249dd866f28b4444904fd157af009dd9989b0a4
tinytoots <wangchen2018n@163.com>
2021-10-14 15:38:42 -0400

    add responsive layout for header

---
+++ authright/src/components/Header/Header2.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./Header2.css";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+import "antd/dist/antd.css";
+import Logo from "../../img/Authright_logo_white_text.png";
+
+
+export default class Header extends Component {
+
+  render() {
+    return (
+      <div>
+        <header className="header-bg">
+        {/* <h1 class="logo">Logo</h1> */}
+        <NavLink to="/home">
+            <img className="header-logo" src={Logo} width={160} alt=""/>
+        </NavLink>
+        <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+        <nav className="header-nav">
+          <ul>
+            <li><NavLink to="/home">Home</NavLink></li>
+            <li><NavLink to="/services/iam">Service</NavLink></li>
+            <li><NavLink to="/customer">Customer</NavLink></li>
+            <li><NavLink to="/partners">Partners</NavLink></li>
+            <li><NavLink to="/about-us">About</NavLink></li>
+          </ul>
+        </nav>
+        <label for="nav-toggle" className="nav-toggle-label">
+          <span></span>
+        </label>
+        </header>
+
+        {/* <div class="content">
+          <h2>Your content would go here</h2>
+        </div> */}
+      </div>
+    );
+  }
+}
