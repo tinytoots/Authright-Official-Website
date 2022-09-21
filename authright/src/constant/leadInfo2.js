98c938042f8ad91b4696723b2b3a03ecde75c0e0
tinytoots <wangchen2018n@163.com>
2022-09-21 12:48:41 -0400

    update jay profile

--- authright/src/constant/leadInfo2.js
+++ authright/src/constant/leadInfo2.js
@@ -2,7 +2,7 @@ export const leadInfo = {
     name: "Jay Zheng",
     title: "CTO, Co-Founder",
     linkedin: "https://www.linkedin.com/in/jay-zheng-81ab0a3",
-    pic: "https://tva1.sinaimg.cn/large/e6c9d24egy1h6eomt74tfj20u013ndlp.jpg",
+    pic: "https://tva1.sinaimg.cn/large/e6c9d24egy1h6epha7bn5j20u00u0ado.jpg",
     description:
       ["Jay believes in technology and teamwork. After receiving his Master's degree in physics, Jay decided to put his mathematics and physics training into use as a computer engineer; a wise decision which has resulted in a fruitful journey filled with dynamic experiences thus far." ,
       "Following a short career as a freelance consultant after quitting his full-time job as a principal architect at IDG, Jay met Don, and joined the OSC technology team as the VP of engineering. Three years later, they co-founded Authright in 2017.",
