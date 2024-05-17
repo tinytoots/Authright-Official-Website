32f62e6ad80e7b96ca3113dbc0e41dcf7a3a4ef9
tinytoots <wangchen2018n@163.com>
2024-05-17 12:39:52 -0400

    Directors update

--- authright/src/pages/about/advisors/advisors.js
+++ authright/src/pages/about/advisors/advisors.js
@@ -11,14 +11,14 @@ export default function Advisoryboard(props) {
   return (
     <div className="advisor-bg">
       {/* <div className="leader-title">Authright Founders</div> */}
-      {/* <div className="about-leadership-ceo1">
-        <LeadCard leadInfo={leads} />
-      </div> */}
       {/* <div className="leader-title">Co-Founder</div> */}
-      {/* <div className="about-leadership-ceo2">
-        <LeadCard leadInfo={leadsScond} />
-      </div> */}
       <div className="leader-title">Authright Board of Directors</div>
+      <div className="about-leadership-ceo1">
+        <LeadCard leadInfo={leads} />
+      </div>
+      <div className="about-leadership-ceo2">
+        <LeadCard leadInfo={leadsScond} />
+      </div>
       <div className="about-leadership-ceo1">
         <LeadCard leadInfo={boardDirect} />
       </div>
